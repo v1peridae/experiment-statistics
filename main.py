4ab7d20367ed1d4cf59910333bcec465a7738445
louisa <82815171+viperidaee@users.noreply.github.com>
2023-08-02 01:55:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#TASK 1
 student_age = []
 school_house = []
 reaction_time = []
@@ -8,41 +7,42 @@ mars_count = 0
 saturn_reaction_time = 0
 mars_reaction_time = 0
 
-#TASK 2
-for i in range(1, 4):
-  print(f"PARTICIPANT {i}'S TURN"
-        "\n")
+for i in range(1, 651):
+  print(f"PARTICIPANT {i}'S TURN""\n")
   house_input = input("What house are you in? >>>")
   school_house.append(house_input)
 
   if house_input == "Saturn" or house_input == "saturn" or house_input == "satrun" or house_input == "Satrun":
     saturn_count += 1
-    saturn_reaction_time_input = float(
-        input("How long did your reaction take (in seconds)? >>>"))
+    
+    saturn_reaction_time_input = float(input("How long did your reaction take (in seconds)? >>>"))
     reaction_time.append(saturn_reaction_time_input)
+    
     saturn_reaction_time += saturn_reaction_time_input
     avg_saturn_reaction_time = saturn_reaction_time / saturn_count
 
   elif house_input == "Mars" or house_input == "mars":
     mars_count += 1
-    mars_reaction_time_input = float(
-        input("How long did your reaction take (in seconds)? >>>"))
+    
+    mars_reaction_time_input = float(input("How long did your reaction take (in seconds)? >>>"))
     reaction_time.append(mars_reaction_time_input)
+    
     mars_reaction_time += mars_reaction_time_input
     avg_mars_reaction_time = mars_reaction_time / mars_count
   else:
     print("Enter a house between Saturn or Mars.")
-
+    break
+    
   age_input = int(input("How old are you? >>>"))
   if age_input > 16 or age_input < 12:
     print("You must be between 16 and 12 to participate.")
+    break
   else:
     student_age.append(age_input)
   print("\n")
 
 reaction_time.sort()
-print("\n"
-      "The slowest reaction time was", reaction_time[-1])
+print("\n""The slowest reaction time was", reaction_time[0])
 
 avg_reaction_time = sum(reaction_time) / len(reaction_time)
 print("\n"
